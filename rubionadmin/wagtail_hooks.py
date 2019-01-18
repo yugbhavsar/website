2d823b49b91d858a4e25a950d61fe5e16e62011f
patta42 <patrick.happel@rub.de>
2019-01-18 09:34:22 +0100

    updated to django.urls

--- rubionadmin/wagtail_hooks.py
+++ rubionadmin/wagtail_hooks.py
@@ -4,7 +4,7 @@ from .admin_panels import (
 )
 
 from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.utils.translation import ugettext as _
 
 from notifications.admin_panels import NotificationPanel 
