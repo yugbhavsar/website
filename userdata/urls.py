3874a6d20a8a9c0a0ca758433282bd3a7b95a3e5
patta42 <patrick.happel@rub.de>
2019-01-18 13:18:26 +0100

    renamed pattern

--- userdata/urls.py
+++ userdata/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from .admin_views import add_safety_relation
 
-urls = [
+app_name = 'userdata'
+
+urlpatterns = [
     url(r'^userdata/safetyinstructionuserrelation/add/(\w+)/(\d+)/(\d+)/$', add_safety_relation, name='safetyinstruction_addwithpreselection'),
 ]
