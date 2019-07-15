2fc23f9fecf5ea4ae43770cb8af80f5975529a0d
patta42 <patrick.happel@rub.de>
2019-07-15 18:49:01 +0200

    updated to use the new modal workflow API

--- rubion/static/js/wagtailadmin/reject.js
+++ rubion/static/js/wagtailadmin/reject.js
@@ -3,6 +3,7 @@ function createRejectionModal() {
     btns.each( 
 	function( ){
 	    var form = $(this).parent();
+	    console.log(form);
 	    form.submit(
 		function( evt ) {
 
@@ -11,14 +12,40 @@ function createRejectionModal() {
 
 		    // get the id 
 		    var url_parts = form.attr('action').split('/');
-		    var r_url = window.rubionadmin.reject_form_url + url_parts[3] + '/'
+		    var r_url = window.rubionadmin.reject_form_url + url_parts[3] + '/';
 		    ModalWorkflow({
 			url : r_url,
 			responses : {
 
+			},
+			onload: {
+			    'success' : function(modal, jsonData){
+				window.location.href = jsonData['wagtailadmin_home_url'];
+			    },
+			    'show' : function(modal, jsonData){
+
+				$('form.rejection-form', modal.body).submit(function() {
+				    var formdata = new FormData(this);
+				    $.ajax({
+					url: this.action,
+					data: formdata,
+					processData: false,
+					contentType: false,
+					type: 'POST',
+					dataType: 'text',
+					success: function(response, status, jqXHR){
+					    console.log(response);
+					    modal.loadResponseText(response, status, jqXHR);
+					}
+				    });
+    				    return false;
+				});
+			    }
+
+			    
+
 			}
-		    }
-		    );
+		    }); // ModalWorkflow
 		    
 		    return false;
 		}
