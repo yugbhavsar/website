74a88296f93db1d58586f0ade49d25b74a0de5f5
patta42 <patrick.happel@rub.de>
2019-07-17 18:47:28 +0200

    added waitlist widget

--- courses/widgets.py
+++ courses/widgets.py
@@ -1,8 +1,27 @@
-from django.forms.widgets import Select
 from .attendees import get_attendee_choices
+
+from website.widgets import StyledCheckbox
+
+from django.forms.widgets import Select
+
+
 
 class AttendeeSelectWidget( Select ):
     def __init__( self, *args, **kwargs ):
         super(AttendeeSelectWidget, self).__init__( *args, **kwargs )
 
         self.choices = get_attendee_choices()
+
+
+class WaitlistCheckboxWidget( StyledCheckbox ):
+    template_name = 'courses/forms/widgets/waitlist_checkbox.html'
+
+    
+    def __init__( self, *args, **kwargs ):
+        self.course = None
+        super(WaitlistCheckboxWidget, self).__init__( *args, **kwargs)
+
+    def get_context( self, name, value, attrs ):
+        context = super(WaitlistCheckboxWidget, self).get_context(name, value, attrs)
+        context['widget']['course'] = self.course
+        return context
