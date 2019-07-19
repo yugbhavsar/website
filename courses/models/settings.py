d2cab8b7f941f84ffbd288c93067dcf904e3cf3f
patta42 <patrick.happel@rub.de>
2019-07-19 10:29:37 +0200

    added _

--- courses/models/settings.py
+++ courses/models/settings.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext as _
 
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.contrib.settings.models import BaseSetting, register_setting
