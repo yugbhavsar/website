e31514fbbeeb281a26b48409584ffa8adf3d8d90
patta42 <patrick.happel@rub.de>
2019-07-19 10:20:36 +0200

    initital

---
+++ courses/models/snippets.py
@@ -0,0 +1,27 @@
+from .attendees import CourseAttendee
+from django.db import models
+
+@register_snippet
+class CourseParticipationValidation( models.Model ):
+    attendee = models.ForeignKey(
+        CourseAttendee,
+        on_delete = models.CASCADE,
+        editable = False
+    )
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        null = True,
+        blank = True,
+        editable = False
+    )
+    created_at = models.DateTimeField(
+        auto_now_add = True
+    )
+
+    def validate( self ):
+        self.attendee.validate()
+        self.uuid = None
+        self.save()
+        
+    def __str__( self ):
+        return "{}: {} ({})".format(self.uuid, self.attendee.full_name, self.created_at)
