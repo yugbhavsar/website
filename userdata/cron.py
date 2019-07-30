62f9d9db9bbc6bb3f25f0944142db5e031f26572
patta42 <patrick.happel@rub.de>
2019-07-30 10:28:59 +0200

    continued...

---
+++ userdata/cron.py
@@ -0,0 +1,48 @@
+import datetime
+
+from dateutil.relativedelta import relativedelta
+
+from django.db.models import Q
+
+from django_cron import CronJobBase, Schedule
+
+import logging
+
+from notifications.models import SafetyInstructionExpiredNotifications
+
+from userdata.safety_instructions import get_instruction_information
+from userinput.models import RUBIONUser
+
+
+from website.models import EMailText
+
+class WarnSafetyInstruction( CronJobBase ):
+    RUN_EVERY_MINS = 24 * 60 * 7 # once a week
+
+    schedule = Schedule(run_every_mins = RUN_EVERY_MINS)
+
+    code = 'userdata.warn_safety_instructions'
+
+    def do( self ):
+        self.today = datetime.datetime.today()
+        self.next_month =  self.today + relativedelta(months = +1)
+        self.two_weeks_ago = self.today + relativedelta(weeks=-2)
+        self.one_weeks_ago = self.today + relativedelta(weeks=-1)
+
+        for ru in RUBIONUser.objects.live().all():
+            expired = []
+            soon_expired = []
+            info = get_instruction_information(ru)
+            for si, i in info.items():
+                if i.required:
+                    if i.last < self.today:
+                        expired.append(si)
+                    elif i.last < self.next_month:
+                        soon_expired.append(si)
+            if expired or soon_expired:
+                mail = EMailText.objects.get(identifier = 'warning.safety_instruction')
+        
+
+    def needs_to_be_sent( self, ru ):
+        # We need to check every 
+        notification = SafetyInstructionExpiredNotifications.objects.filter(
